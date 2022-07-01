e405078c4b160b7850f8044c7fdbc85651be025a
projectjh <jhhhh9201@gmail.com>
2022-07-01 17:29:07 +0900

    upload

--- JS/test.js
+++ JS/test.js
@@ -449,6 +449,139 @@
 // console.log(false || 'Dog');
 // console.log('Cat' || false);
 
-console.log('Cat' && 'Dog');
-console.log(false && 'Dog');
-console.log('Cat' && false);
+// console.log('Cat' && 'Dog');
+// console.log(false && 'Dog');
+// console.log('Cat' && false);
+
+// ◻️ 10-01 [객체 리터럴]
+// var person = {
+//     name: 'Lee',                                            // 프로퍼티
+//     age: 30,
+//     sayHello: function (){                                  // 메서드
+//         var name = 'aaaa'
+//         console.log(`Hello! My name is ${this.name}.`);     // ✨this → 객체 자신을 가르킴 (내부에서 접근할때 꼭 필요)
+//         console.log(`Hello! My age is ${this.age}.`);
+//         console.log(`Hello! My name is ${name}.`);          // 함수 안의 name을 가르킴
+//         console.log(`Hello! My tel is ${this.tel}.`);
+//         str = `
+//             <table border="1" align="center">
+//                 <tr>
+//                     <th>Name</th>
+//                     <th>Age</th>
+//                     <th>Tel</th>
+//                 </tr>
+//                 <tr>
+//                     <td>${this.name}</td>
+//                     <td>${this.age}</td>
+//                     <td>${this.tel}</td>
+//                 </tr>
+//             </table>
+//         `
+//         return document.write(str);
+//         // return;                                          // 호출한 문장을 자동으로 복귀하라. 복귀할 값이 없기 때문에 undefined 출력
+//         // return 1;                                        // 함수를 사용하고 1 값을 저장하여 복귀
+//     }
+//     // sayHello(){                                                // function 키워드 생략한 축약표현 (ES6)
+//     //     console.log(`Hello! My name is ${this.name}.`);
+//     //     console.log(`Hello! My age is ${this.age}.`);
+//     //     console.log(`Hello! My tel is ${this.tel}.`);
+//     // }
+// };
+
+// // console.log(typeof person);
+// // console.log(person);                                        
+// // console.log(person.name);                                   // 프로퍼티 value 호출
+// // console.log(person['name']);
+// // console.log(person["name"]);
+// // console.log(person[`name`]);
+// // console.log(person.sayHello());                             // 메서드 실행 형식
+
+// person.tel = "010-1111-1111"                                   // 객체 내부 구조 변경 가능 → 프로퍼티 동적 생성
+// console.log(person);
+// console.log(person.tel);
+// //console.log(person.sayHello());
+// person.sayHello();                                              // table 제작 내용을 html에 호출
+
+
+// Q. 성적 데이터를 객체로 형성 
+var person = {      // person은 8개의 특성과 3개의 메서드를 가진 객체
+    hakbun: '',
+    name: '',
+    kor: 0,
+    eng: 0,
+    math: 0,
+    sum: 0,
+    avg: 0.0,
+    grade: '',
+
+    getSum() {
+        this.sum = this.kor + this.eng + this.math;
+    },
+
+    getAvg() {
+        this.avg = this.sum / 3
+    },
+
+    getGrade() {
+        switch (parseInt(this.avg / 10)) {
+            case 10:
+            case 9:
+                this.grade = '수';
+                break;
+            case 8:
+                this.grade = '우';
+                break;
+            case 7:
+                this.grade = '미';
+                break;
+            case 6:
+                this.grade = '양';
+                break;
+            default:
+                this.grade = '가';
+                break;      
+        }
+    },
+    printData() {
+        str = `
+            <table border="1" align="center">
+                <tr>
+                    <th>학번</th>
+                    <th>이름</th>
+                    <th>국어</th>
+                    <th>영어</th>
+                    <th>수학</th>
+                    <th>총점</th>
+                    <th>평균</th>
+                    <th>등급</th>
+                </tr>
+                <tr>
+                    <td>${this.hakbun}</td>
+                    <td>${this.name}</td>
+                    <td>${this.kor}</td>
+                    <td>${this.eng}</td>
+                    <td>${this.math}</td>
+                    <td>${this.sum}</td>
+                    <td>${this.avg}</td>
+                    <td>${this.grade}</td>
+                </tr>
+            </table>
+        `
+        return document.write(str);
+    }
+}
+
+// 값 입력받기
+person.hakbun = prompt('학번 입력');
+person.name = prompt('이름 입력');
+person.kor = parseInt(prompt('국어 점수 입력'));
+person.eng = parseInt(prompt('영어 점수 입력'));
+person.math = parseInt(prompt('수학 점수 입력'));
+
+// 메서드 호출
+person.getSum();
+person.getAvg();
+person.getGrade();
+person.printData();
+
+
