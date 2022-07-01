e22b08c71fb940d8d0925366fea65c40e93d5245
projectjh <jhhhh9201@gmail.com>
2022-07-01 19:42:59 +0900

    commit

---
+++ JS/self-test.js
@@ -0,0 +1,10 @@
+const prefix = 'prop';
+let i = 0;
+
+const obj = {
+    [`${prefix}-${++i}`]: i,
+    [`${prefix}-${++i}`]: i,
+    [`${prefix}-${++i}`]: i
+};
+
+console.log(obj);
--- JS/test.js
+++ JS/test.js
@@ -496,7 +496,7 @@
 // // console.log(person[`name`]);
 // // console.log(person.sayHello());                             // 메서드 실행 형식
 
-// person.tel = "010-1111-1111"                                   // 객체 내부 구조 변경 가능 → 프로퍼티 동적 생성
+// person.tel = "010-1111-1111"                                   // 객체 내부 구조 변경 가능 → 프로퍼티 동적
 // console.log(person);
 // console.log(person.tel);
 // //console.log(person.sayHello());
@@ -504,84 +504,98 @@
 
 
 // Q. 성적 데이터를 객체로 형성 
-var person = {      // person은 8개의 특성과 3개의 메서드를 가진 객체
-    hakbun: '',
-    name: '',
-    kor: 0,
-    eng: 0,
-    math: 0,
-    sum: 0,
-    avg: 0.0,
-    grade: '',
+// var person = {      // person은 8개의 특성과 3개의 메서드를 가진 객체
+//     hakbun: '',
+//     name: '',
+//     kor: 0,
+//     eng: 0,
+//     math: 0,
+//     sum: 0,
+//     avg: 0.0,
+//     grade: '',
 
-    getSum() {
-        this.sum = this.kor + this.eng + this.math;
-    },
+//     getSum() {
+//         this.sum = this.kor + this.eng + this.math;
+//     },
 
-    getAvg() {
-        this.avg = this.sum / 3
-    },
+//     getAvg() {
+//         this.avg = this.sum / 3
+//     },
 
-    getGrade() {
-        switch (parseInt(this.avg / 10)) {
-            case 10:
-            case 9:
-                this.grade = '수';
-                break;
-            case 8:
-                this.grade = '우';
-                break;
-            case 7:
-                this.grade = '미';
-                break;
-            case 6:
-                this.grade = '양';
-                break;
-            default:
-                this.grade = '가';
-                break;      
-        }
-    },
-    printData() {
-        str = `
-            <table border="1" align="center">
-                <tr>
-                    <th>학번</th>
-                    <th>이름</th>
-                    <th>국어</th>
-                    <th>영어</th>
-                    <th>수학</th>
-                    <th>총점</th>
-                    <th>평균</th>
-                    <th>등급</th>
-                </tr>
-                <tr>
-                    <td>${this.hakbun}</td>
-                    <td>${this.name}</td>
-                    <td>${this.kor}</td>
-                    <td>${this.eng}</td>
-                    <td>${this.math}</td>
-                    <td>${this.sum}</td>
-                    <td>${this.avg}</td>
-                    <td>${this.grade}</td>
-                </tr>
-            </table>
-        `
-        return document.write(str);
+//     getGrade() {
+//         switch (parseInt(this.avg / 10)) {
+//             case 10:
+//             case 9:
+//                 this.grade = '수';
+//                 break;
+//             case 8:
+//                 this.grade = '우';
+//                 break;
+//             case 7:
+//                 this.grade = '미';
+//                 break;
+//             case 6:
+//                 this.grade = '양';
+//                 break;
+//             default:
+//                 this.grade = '가';
+//                 break;      
+//         }
+//     },
+//     printData() {
+//         str = `
+//             <table border="1" align="center">
+//                 <tr>
+//                     <th>학번</th>
+//                     <th>이름</th>
+//                     <th>국어</th>
+//                     <th>영어</th>
+//                     <th>수학</th>
+//                     <th>총점</th>
+//                     <th>평균</th>
+//                     <th>등급</th>
+//                 </tr>
+//                 <tr>
+//                     <td>${this.hakbun}</td>
+//                     <td>${this.name}</td>
+//                     <td>${this.kor}</td>
+//                     <td>${this.eng}</td>
+//                     <td>${this.math}</td>
+//                     <td>${this.sum}</td>
+//                     <td>${this.avg}</td>
+//                     <td>${this.grade}</td>
+//                 </tr>
+//             </table>
+//         `
+//         return document.write(str);
+//     }
+// }
+
+// // 값 입력받기
+// person.hakbun = prompt('학번 입력');
+// person.name = prompt('이름 입력');
+// person.kor = parseInt(prompt('국어 점수 입력'));
+// person.eng = parseInt(prompt('영어 점수 입력'));
+// person.math = parseInt(prompt('수학 점수 입력'));
+
+// // 메서드 호출
+// person.getSum();
+// person.getAvg();
+// person.getGrade();
+// person.printData();
+
+var person = {
+    name: 'Lee',
+    printPerson() {
+        return document.write(`name = ${this.name}, age = ${this.age}`);
     }
 }
 
-// 값 입력받기
-person.hakbun = prompt('학번 입력');
-person.name = prompt('이름 입력');
-person.kor = parseInt(prompt('국어 점수 입력'));
-person.eng = parseInt(prompt('영어 점수 입력'));
-person.math = parseInt(prompt('수학 점수 입력'));
+person.age = 20;
+//document.write(`name = ${person.name}, age = ${person.age}`);
+person.printPerson();
 
-// 메서드 호출
-person.getSum();
-person.getAvg();
-person.getGrade();
-person.printData();
+
+
 
 
