7fd1c6f4c48a3452b8b5b1edabbdaa6649975ddb
projectjh <jhhhh9201@google.com>
2022-11-30 20:47:19 +0900

    20221130

---
+++ project_backup/jh-2/projectGS/src/components/user/MyPage.js
@@ -0,0 +1,10 @@
+const MyPage = () => {
+  return (
+    <div id="MyPage">
+      <div className="memberSection">
+        <div className="sub-title"><h2>마이페이지</h2></div>
+      </div>
+    </div>
+  );
+};
+export default MyPage;
